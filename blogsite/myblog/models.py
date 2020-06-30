0708f1ad366ca9cb719451f8931fa043a44b852d
abdul_waris <siraluda2020@gmail.com>
2020-06-29 22:38:08 -0230

    Initial commit

--- blogsite/myblog/models.py
+++ blogsite/myblog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
-from django.contrib.auth.models import User
 from django.shortcuts import reverse
+from django.contrib.auth.models import User
+
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
