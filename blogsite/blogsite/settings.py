ee2c5f14150778b51a2efe4db31fa26177240010
abdul_waris <siraluda2020@gmail.com>
2020-07-24 21:01:30 -0230

    settings

--- blogsite/blogsite/settings.py
+++ blogsite/blogsite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('BLOG_APP_SECRET_KEY', 'blogsite.settings')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get('LINODE_SERVER_IP', 'blogsite.settings'),]
+ALLOWED_HOSTS = [os.environ.get('LINODE_SERVER_IP', 'blogsite.settings'), '*']
 
 
 # Application definition
